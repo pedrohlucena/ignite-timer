3ffc30d4ae2c5f26633d79bd6456b5a07112d284
Pedro H. Lucena <pedrohlucena413@gmail.com>
2023-07-02 08:12:17 -0300

    eslint config

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle, css } from "styled-components";
+import { createGlobalStyle, css } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
     ${({ theme }) => css`
@@ -10,21 +10,21 @@ export const GlobalStyle = createGlobalStyle`
 
       :focus {
         outline: 0;
-        box-shadow: 0 0 0 2px ${theme["green-500"]};
+        box-shadow: 0 0 0 2px ${theme['green-500']};
       }
 
       body {
-        background: ${theme["gray-900"]};
-        color: ${theme["gray-300"]};
+        background: ${theme['gray-900']};
+        color: ${theme['gray-300']};
       }
 
       body,
       input,
       textarea,
       button {
-        font-family: "Roboto", sans-serif;
+        font-family: 'Roboto', sans-serif;
         font-weight: 400;
         font-size: 1rem;
       }
     `}
-`;
+`
