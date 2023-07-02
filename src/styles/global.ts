b6b42eba8c66dd19051b256129265599f15b0dcb
Pedro H. Lucena <pedrohlucena413@gmail.com>
2023-07-02 07:24:51 -0300

    create app global style

---
+++ src/styles/global.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle, css } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    ${({ theme }) => css`
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: 0;
+      }
+
+      :focus {
+        outline: 0;
+        box-shadow: 0 0 0 2px ${theme["green-500"]};
+      }
+
+      body {
+        background: ${theme["gray-900"]};
+        color: ${theme["gray-300"]};
+      }
+
+      body,
+      input,
+      textarea,
+      button {
+        font-family: "Roboto", sans-serif;
+        font-weight: 400;
+        font-size: 1rem;
+      }
+    `}
+`;
