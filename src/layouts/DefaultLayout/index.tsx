95c1a8a09ebee0c387f5be89999838288549bbff
Pedro H. Lucena <pedrohlucena413@gmail.com>
2023-07-02 20:57:00 -0300

    create header component

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router-dom'
-import { Header } from '../components/Header'
+import { Header } from '../../components/Header'
+import * as S from './styles'
 
 export function DefaultLayout() {
   return (
-    <div>
+    <S.LayoutContainer>
       <Header />
       <Outlet />
-    </div>
+    </S.LayoutContainer>
   )
 }
